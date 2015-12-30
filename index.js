1be62234449e09bf4a580441d5600fd6e534e7be
Connor Brathwaite <connor.brathwaite@gmail.com>
2015-12-30 14:46:45 -0500

    fixed jade anchor tag

--- index.js
+++ index.js
@@ -12,20 +12,20 @@ const app = express()
   .set(`views`, __dirname + `/views`)
   .use(express.static(__dirname + `/static`))
   .set(`view engine`, `jade`)
-  .engine(`jade`, require(`jade`).__express)
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
   .get(`/`, (req, res) => res.render(`index`))
   .post(`/`, (req, res) => {
     let url = req.body.url
     let id = shortid.generate()
-    client.set(id, url, () => res.render(`output`, { id: id, base: base }))
+    client.set(id, url, () => res.render(`output`, {
+      link: `${base}/${id}`
+    }))
   })
   .get(`/:id`, (req, res) => {
     let id = req.params.id.trim()
     client.get(id, (err, reply) => {
       if(!err && reply) {
-
         res.status(301)
         res.set(`Location`, reply)
         res.send()
